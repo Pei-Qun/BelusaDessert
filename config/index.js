d33402cfd8684624ed36722d846dfc769f84f391
Pei-Qun <nancy016071@gmail.com>
2019-02-10 13:01:29 +0800

    assetsPublicPath:BelusaDessert.dist

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/belusa/dist/',
+    assetsPublicPath: '/BelusaDessert/dist/',
 
     /**
      * Source Maps
